75ebf0b31628ce9c6b1223a70d793e0235d9072f
Haritha R <harithalaks1998@gmail.com>
2024-07-01 16:07:26 +0530

    Add project files.

---
+++ No Argument and No Return Type/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_Argument_and_No_Return_Type
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            studentInfo();
+            Console.ReadKey();
+        }
+        public static void studentInfo()
+    // void = no return
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string ageInput = Console.ReadLine();
+            
+           
+            
+        }
+    }
+}
